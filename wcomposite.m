166f197342335b4bc9c657d8b51215659cf53973
YMI33 <731897990@qq.com>
2017-09-24 14:39:52 +0800

    统计检验修正

--- wcomposite.m
+++ wcomposite.m
@@ -4,7 +4,7 @@ function [difference sigcrit]=wcomposite(x,y,alpha0)
 %其中index为一维指数，field为加权合成分析的物理量场，必须为三维变量，第三维为时间维，
 %alpha0是统计检验标准，如0.95表示95%统计检验,默认采用双侧检验，蒙特卡洛检验采用200个样本
 %输出量WCA为三维变量，前两维与输入的field相同，第三维为不同异常结果，正异常结果为WCA(:,:,1),负异常为WCA(:,:,2)，合成差为WCA(:,:,3)
-%significance为显著检验结果，significance(:,:,1)为置信上界，significance(:,:,2)为置信下界,WCA值高于上界或低于下界的即为通过检验
+%significance维数与WCA相同，为显著检验结果,为1即该格点通过检验，为0则该格点未通过检验
 sizlen=size(x);
 if sizlen(1)==1
     x=x';
@@ -56,7 +56,7 @@ for k=1:N
 end
 %计算检验标准
 diff_test(:,:,:,3)=diff_test(:,:,:,1)-diff_test(:,:,:,2);
-sigcrit=nan(sizy(1),sizy(2),3);
+sigcrit=zeros(sizy(1),sizy(2),3);
 for k=1:3
     for k1=1:sizy(1)
         for k2=1:sizy(2)
@@ -64,9 +64,9 @@ for k=1:3
          sigcrit1=diff_test_sig(1,1,floor(N*alpha0)+1);
          sigcrit2=diff_test_sig(1,1,floor(N*(1-alpha0))-1);
          if difference(k1,k2,k)<sigcrit1
-             sigcrit(k1,k2,k)=sigcrit1;
+             sigcrit(k1,k2,k)=1;
          elseif difference(k1,k2,k)>sigcrit2
-             sigcrit(k1,k2,k)=sigcrit2;
+             sigcrit(k1,k2,k)=1;
         end
     end
 end
