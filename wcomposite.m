0b5c9d1673455c6c9673591d2cb3c20a4fff0e38
Zhiang Xie <xiezhiang@lasg.iap.ac.cn>
2019-01-13 11:58:26 +0800

    add English notation
    Add English notation for the program

--- wcomposite.m
+++ wcomposite.m
@@ -1,10 +1,30 @@
 function [difference sigcrit]=wcomposite(x,y,alpha0)
-%计算加权合成分析的函数，采用双侧蒙特卡洛检验
-%函数形式 [WCA significance]=wcomposite(index,field,alpha0)
-%其中index为一维指数，field为加权合成分析的物理量场，必须为三维变量，第三维为时间维，
-%alpha0是统计检验标准，如0.95表示95%统计检验,默认采用双侧检验，蒙特卡洛检验采用200个样本
-%输出量WCA为三维变量，前两维与输入的field相同，第三维为不同异常结果，正异常结果为WCA(:,:,1),负异常为WCA(:,:,2)，合成差为WCA(:,:,3)
-%significance维数与WCA相同，为显著检验结果,为1即该格点通过检验，为0则该格点未通过检验
+%
+% Updated by Zhiang Xie, 13 Jan 2019
+%
+% Function for Weighted Composite Analysis (WCA) with Monte Carlo two-tails test
+% 
+% Ex. [WCA significance] = wcomposite(index,field,alpha0)
+% Input: 
+% where [index] represents the 1d index to tell phase of event, 
+% [field] represents physical field, which must be 3d variable and time is the rightmost dimension
+% [alpha0] is significant criterion, e.g. 0.95 for 95% siginificant test, 
+% 200 samples are defaultly used in Monte Carlo test
+% Output:
+% where WCA is 3d variables, the first two dimensions same as input field 
+% [WCA(:,:,1)] represents the result in positive phase, [WCA(:,:,2)] for negative and [WCA(:,:,3)] for difference
+% [significance] shares the same dimension with WCA, representing the significant test result, 
+% 1 for significant and 0 for insignificant
+%
+% 中文注释：
+% 计算加权合成分析的函数，采用双侧蒙特卡洛检验
+% 函数形式 [WCA significance]=wcomposite(index,field,alpha0)
+% 其中index为一维指数，field为加权合成分析的物理量场，必须为三维变量，第三维为时间维，
+% alpha0是统计检验标准，如0.95表示95%统计检验,默认采用双侧检验，蒙特卡洛检验采用200个样本
+% 输出量WCA为三维变量，前两维与输入的field相同，第三维为不同异常结果，正异常结果为WCA(:,:,1),负异常为WCA(:,:,2)，合成差为WCA(:,:,3)
+% significance维数与WCA相同，为显著检验结果,为1即该格点通过检验，为0则该格点未通过检验
+%
+
 sizlen=size(x);
 if sizlen(1)==1
     x=x';
@@ -13,21 +33,22 @@ else
     sizlen=sizlen(1);
 end
 
-%双侧检验,单侧检验需注释下面的语句
+% Two-tail test, notate following section for one-tail test
 %-----------------------
 alpha0=(1+alpha0)/2;
 %-----------------------
 
 sizy=size(y);
 siznum=3;
-%计算合成结果
+
+% Composite sumation
 x_anomal=x-mean(x);
 y_mean=mean(y,siznum);
 y_anomal=y-repmat(y_mean,[ones(1,siznum-1) sizy(siznum)]);
 p=find(x_anomal>0);
 n=find(x_anomal<0);
 
-%difference第三维分别为：1.positive part; 2.negative part; 3. differnece
+% Difference last dimension：1.positive part; 2.negative part; 3. differnece
 difference=nan(sizy(1),sizy(2),3);
 for k1=1:sizy(1)
     for k2=1:sizy(2)
@@ -37,10 +58,10 @@ for k1=1:sizy(1)
 end
 difference(:,:,3)=difference(:,:,1)-difference(:,:,2);
 
-%计算统计检验
-N=200;%蒙特卡洛随机试验次数
+% Significant test
+N=200; % ensample number 
 diff_test=nan(sizy(1),sizy(2),N,3);
-%计算随机序列的加权合成分析结果
+% WCA for every sample
 for k=1:N
   order=randperm(sizlen);
   x_test=x(order);
@@ -54,15 +75,15 @@ for k=1:N
         end
     end
 end
-%计算检验标准
+% The significant interval
 diff_test(:,:,:,3)=diff_test(:,:,:,1)-diff_test(:,:,:,2);
 sigcrit=zeros(sizy(1),sizy(2),3);
 for k=1:3
     for k1=1:sizy(1)
         for k2=1:sizy(2)
          diff_test_sig=sort(diff_test(k1,k2,:,k),3);
-         sigcrit1=diff_test_sig(1,1,floor(N*alpha0)+1); %置信上界
-         sigcrit2=diff_test_sig(1,1,floor(N*(1-alpha0))-1); %置信下界
+         sigcrit1=diff_test_sig(1,1,floor(N*alpha0)+1); % upper bound
+         sigcrit2=diff_test_sig(1,1,floor(N*(1-alpha0))-1); % bottom bound
          if difference(k1,k2,k)>sigcrit1
              sigcrit(k1,k2,k)=1;
          elseif difference(k1,k2,k)<sigcrit2
